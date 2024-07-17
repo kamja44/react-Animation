b1e77a8433553bcfe7b93c510da6e56ccd6b4fe0
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-07-18 00:01:22 +0900

    motionvalues

--- src/App.tsx
+++ src/App.tsx
@@ -1,155 +1,49 @@
 import styled from "styled-components";
-import { delay, motion } from "framer-motion";
-import { useRef } from "react";
+import {
+  delay,
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { useEffect, useRef } from "react";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
-// const Box = styled(motion.div)`
-//   width: 200px;
-//   height: 200px;
-//   display: grid;
-//   grid-template-columns: repeat(2, 1fr);
-//   background-color: rgba(255, 25, 255, 0.2);
-//   border-radius: 15px;
-//   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-// `;
-
-/* Basic
- * 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 15px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const myVars = {
-  start: {
-    scale: 0,
-  },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
-};
- */
-
-/** Variants
- * 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 25, 255, 0.2);
-  border-radius: 15px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-const Circle = styled(motion.div)`
-  background-color: wheat;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  height: 70px;
-  width: 70px;
-  border-radius: 35px;
-  place-self: center;
-`;
-
-const boxVras = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.2,
-      staggerChildren: 0.2,
-    },
-  },
-};
-const circleVars = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
-};
- */
-/**Gesture
- * Gesture
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   background-color: rgba(255, 255, 255, 1);
-  opacity: 1;
-  border-radius: 15px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-const boxVars = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { borderRadius: 300 },
-};
- */
-
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  opacity: 1;
-  border-radius: 15px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-const boxVars = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { borderRadius: 300 },
-  drag: { backgroundColor: "rgb(46,204,113)", transition: { duration: 3 } },
-};
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
   border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-200, 200], [-360, 360]);
+  useMotionValueEvent(rotateZ, "change", (i) => {
+    console.log(i);
+  });
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
-      {/* Basic Animation */}
-      {/* <Box variants={myVars} initial="start" animate="end" /> */}
-      {/* Variants Animation */}
-      {/* <Box variants={boxVras} initial="start" animate="end">
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-      </Box> */}
-      {/* Gesture */}
-      {/* <Box variants={boxVars} whileHover="hover" whileTap="click" /> */}
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0.3}
-          dragConstraints={biggerBoxRef}
-          variants={boxVars}
-          whileHover="hover"
-          whileDrag="drag"
-          whileTap="click"
-        />
-      </BiggerBox>
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
